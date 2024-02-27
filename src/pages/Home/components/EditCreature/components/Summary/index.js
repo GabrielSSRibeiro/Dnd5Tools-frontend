38ecf799554c0c9916a308516a79c28f3b0deb39
Gabriel Ribeiro <49738156+GabrielSSRibeiro@users.noreply.github.com>
2024-02-26 19:34:09 -0500

    updated creature summary render

--- src/pages/Home/components/EditCreature/components/Summary/index.js
+++ src/pages/Home/components/EditCreature/components/Summary/index.js
@@ -55,162 +55,206 @@ function Summary({ creature, onSave, onDelete, isBasicPack }) {
     [averageLevel, creature.reactions]
   );
 
-  const summaryRows = [
-    {
-      boxes: [
-        {
-          header: "Definições Básicas",
-          items: [
-            { title: "Raridade", value: cc.GetRarity(creature.rarity).display },
-            { title: "Ambiente", value: cc.GetEnviroment(creature.environment)?.display },
-            { title: "Tamanho", value: cc.GetSize(creature.size).display },
-            { title: "Tipo", value: cc.GetType(creature.type).display },
-            { title: "Raça", value: cc.GetRace(creature.race)?.display },
-            { title: "Classe", value: cc.GetClass(creature.class)?.display },
-            { title: "Subclasse", value: cc.GetSubClass(creature.class, creature.subClass)?.display },
-            { title: "Classe", value: cc.GetClass(creature.secondaryClass)?.display },
-            { title: "Subclasse", value: cc.GetSubClass(creature.secondaryClass, creature.secondarySubClass)?.display },
-          ],
-        },
-      ],
-    },
-    {
-      boxes: [
-        {
-          header: "Deslocamentos",
-          items: [
-            { title: "Terrestre", value: cc.GetSpeed(creature.movements.speed)?.display },
-            { title: "Vôo / Planar", value: cc.GetFlying(creature.movements.flying)?.display },
-            { title: "Nataçao", value: cc.GetSwimming(creature.movements.swimming)?.display },
-            { title: "Escavaçao / Escalada", value: cc.GetBurrowing(creature.movements.burrowing)?.display },
-          ],
-        },
-        {
-          header: "Tendências",
-          items: [
-            { title: "Primária", value: cc.GetPrimaryAlignment(creature.primaryAlignment).display },
-            { title: "Secundária", value: cc.GetSecondaryAlignment(creature.secondaryAlignment).display },
-          ],
-        },
-      ],
-    },
-    {
-      boxes: [
-        {
-          header: "Atributos",
-          items: [
-            { title: "Força", value: cc.GetAttribute(creature.attributes.strength).display },
-            { title: "Destreza", value: cc.GetAttribute(creature.attributes.dexterity).display },
-            { title: "Constituiçao", value: cc.GetAttribute(creature.attributes.constitution).display },
-            { title: "Inteligência", value: cc.GetAttribute(creature.attributes.intelligence).display },
-            { title: "Sabedoria", value: cc.GetAttribute(creature.attributes.wisdom).display },
-            { title: "Carisma", value: cc.GetAttribute(creature.attributes.charisma).display },
-          ],
-        },
-      ],
-    },
-    {
-      boxes: [
-        {
-          header: "Valores Básicos",
-          items: [
-            { title: "Vida (PV)", value: creatureHPUpdated },
-            { title: "Ataque", value: creatureAttacksUpdated },
-            { title: "CA", value: cc.GetArmorClass(creature.armorClass).display },
-            { title: "Iniciativa", value: cc.GetInitiative(creature.initiative).display },
-          ],
-        },
-        {
-          header: "Pontos Fracos",
-          items: creature.weakSpots.map((ws) => ({ title: null, value: ws })),
-        },
-      ],
-    },
-    {
-      boxes: [
-        {
-          header: "Resistências e Vulnerabilidades",
-          items: Object.entries(utils.GroupArrayBy(creature.damagesEffectiveness, "value"))
-            .filter((item) => parseInt(item[0]) !== cc.DAMAGES_EFFECTIVENESS.NORMAL)
-            .map((item) => {
-              const value = parseInt(item[0]);
-              const types = item[1];
+  const summaryRows = useMemo(
+    () => [
+      {
+        boxes: [
+          {
+            header: "Definições Básicas",
+            items: [
+              { title: "Raridade", value: cc.GetRarity(creature.rarity).display },
+              { title: "Ambiente", value: cc.GetEnviroment(creature.environment)?.display },
+              { title: "Tamanho", value: cc.GetSize(creature.size).display },
+              { title: "Tipo", value: cc.GetType(creature.type).display },
+              { title: "Raça", value: cc.GetRace(creature.race)?.display },
+              { title: "Classe", value: cc.GetClass(creature.class)?.display },
+              { title: "Subclasse", value: cc.GetSubClass(creature.class, creature.subClass)?.display },
+              { title: "Classe", value: cc.GetClass(creature.secondaryClass)?.display },
+              { title: "Subclasse", value: cc.GetSubClass(creature.secondaryClass, creature.secondarySubClass)?.display },
+            ],
+          },
+        ],
+      },
+      {
+        boxes: [
+          {
+            header: "Deslocamentos",
+            items: [
+              { title: "Terrestre", value: cc.GetSpeed(creature.movements.speed)?.display },
+              { title: "Vôo / Planar", value: cc.GetFlying(creature.movements.flying)?.display },
+              { title: "Nataçao", value: cc.GetSwimming(creature.movements.swimming)?.display },
+              { title: "Escavaçao / Escalada", value: cc.GetBurrowing(creature.movements.burrowing)?.display },
+            ],
+          },
+          {
+            header: "Tendências",
+            items: [
+              { title: "Primária", value: cc.GetPrimaryAlignment(creature.primaryAlignment).display },
+              { title: "Secundária", value: cc.GetSecondaryAlignment(creature.secondaryAlignment).display },
+            ],
+          },
+        ],
+      },
+      {
+        boxes: [
+          {
+            header: "Atributos",
+            items: [
+              { title: "Força", value: cc.GetAttribute(creature.attributes.strength).display },
+              { title: "Destreza", value: cc.GetAttribute(creature.attributes.dexterity).display },
+              { title: "Constituiçao", value: cc.GetAttribute(creature.attributes.constitution).display },
+              { title: "Inteligência", value: cc.GetAttribute(creature.attributes.intelligence).display },
+              { title: "Sabedoria", value: cc.GetAttribute(creature.attributes.wisdom).display },
+              { title: "Carisma", value: cc.GetAttribute(creature.attributes.charisma).display },
+            ],
+          },
+        ],
+      },
+      {
+        boxes: [
+          {
+            header: "Valores Básicos",
+            items: [
+              { title: "Vida (PV)", value: creatureHPUpdated },
+              { title: "Ataque", value: creatureAttacksUpdated },
+              { title: "CA", value: cc.GetArmorClass(creature.armorClass).display },
+              { title: "Iniciativa", value: cc.GetInitiative(creature.initiative).display },
+            ],
+          },
+          {
+            header: "Pontos Fracos",
+            items: creature.weakSpots.map((ws) => ({ title: null, value: ws })),
+          },
+        ],
+      },
+      {
+        boxes: [
+          {
+            header: "Resistências e Vulnerabilidades",
+            items: Object.entries(utils.GroupArrayBy(creature.damagesEffectiveness, "value"))
+              .filter((item) => parseInt(item[0]) !== cc.DAMAGES_EFFECTIVENESS.NORMAL)
+              .map((item) => {
+                const value = parseInt(item[0]);
+                const types = item[1];
 
-              return {
-                title: cc.damageTypes.find((dt) => dt.value === types[0].type).damageEffectiveness.find((de) => de.value === value).display,
-                value: types.map((de) => cc.GetDamageType(de.type).display).join(", "),
-              };
-            }),
-        },
-        {
-          header: "Imune a Condições",
-          items: [{ title: null, value: creature.conditionImmunities.map((ci) => cc.GetCondition(ci).display).join(", ") }].filter(
-            (item) => item.value !== ""
-          ),
-        },
-      ],
-    },
-    {
-      boxes: [
-        {
-          header: "Idiomas",
-          items: [{ title: null, value: creature.languages.map((l) => cc.GetLanguage(l)?.display).join(", ") }].filter((item) => item.value !== ""),
-        },
-        {
-          header: "Sentidos",
-          items: [
-            { title: "Visao no Escuro", value: cc.GetSense(creature.senses.darkVision)?.display },
-            { title: "Sentido Sísmico", value: cc.GetSense(creature.senses.tremorsense)?.display },
-            { title: "Visao Cega", value: cc.GetSense(creature.senses.blindSight)?.display },
-            { title: "Visao Verdadeira", value: cc.GetSense(creature.senses.trueSight)?.display },
-          ].filter((item) => item.value),
-        },
-      ],
-    },
-    {
-      boxes: [
-        {
-          header: "Especiais",
-          items: [
-            { title: "Resistência Lendária", value: cc.GetLegendaryResistency(creature.legendaryResistences)?.display },
-            { title: "Regeneraçao (intensidade)", value: cc.GetRegenerationAmount(creature.regeneration.amount)?.display },
-            ...creature.customSpecials.map((cs) => ({
-              title: cs.description,
-              value: cc.GetCustomSpecialMultiplier(cs.multiplier)?.display ?? "Nenhum",
-            })),
-          ],
-        },
-      ],
-    },
-    {
-      boxes: [
-        {
-          header: "Açoes",
-          items: actionItems,
-        },
-      ],
-    },
-    {
-      boxes: [
-        {
-          header: "Reaçoes",
-          items: reactionItems,
-        },
-        {
-          header: "Aura",
-          items: creature.aura ? [{ title: creature.aura.name, value: cc.GetAuraReach(creature.aura.reach).display }] : [],
-        },
-      ],
-    },
-    {
-      boxes: [
-        {
-          header: "Tesouros",
-          items: creature.treasures.map((t) => ({ title: t.name, value: tc.GetTreasureType(t.type).display })),
-        },
-      ],
-    },
-  ];
+                return {
+                  title: cc.damageTypes.find((dt) => dt.value === types[0].type).damageEffectiveness.find((de) => de.value === value).display,
+                  value: types.map((de) => cc.GetDamageType(de.type).display).join(", "),
+                };
+              }),
+          },
+          {
+            header: "Imune a Condições",
+            items: [{ title: null, value: creature.conditionImmunities.map((ci) => cc.GetCondition(ci).display).join(", ") }].filter(
+              (item) => item.value !== ""
+            ),
+          },
+        ],
+      },
+      {
+        boxes: [
+          {
+            header: "Idiomas",
+            items: [{ title: null, value: creature.languages.map((l) => cc.GetLanguage(l)?.display).join(", ") }].filter((item) => item.value !== ""),
+          },
+          {
+            header: "Sentidos",
+            items: [
+              { title: "Visao no Escuro", value: cc.GetSense(creature.senses.darkVision)?.display },
+              { title: "Sentido Sísmico", value: cc.GetSense(creature.senses.tremorsense)?.display },
+              { title: "Visao Cega", value: cc.GetSense(creature.senses.blindSight)?.display },
+              { title: "Visao Verdadeira", value: cc.GetSense(creature.senses.trueSight)?.display },
+            ].filter((item) => item.value),
+          },
+        ],
+      },
+      {
+        boxes: [
+          {
+            header: "Especiais",
+            items: [
+              { title: "Resistência Lendária", value: cc.GetLegendaryResistency(creature.legendaryResistences)?.display },
+              { title: "Regeneraçao (intensidade)", value: cc.GetRegenerationAmount(creature.regeneration.amount)?.display },
+              ...creature.customSpecials.map((cs) => ({
+                title: cs.description,
+                value: cc.GetCustomSpecialMultiplier(cs.multiplier)?.display ?? "Nenhum",
+              })),
+            ].filter((item) => item.value),
+          },
+        ],
+      },
+      {
+        boxes: [
+          {
+            header: "Açoes",
+            items: actionItems,
+          },
+        ],
+      },
+      {
+        boxes: [
+          {
+            header: "Reaçoes",
+            items: reactionItems,
+          },
+          {
+            header: "Aura",
+            items: creature.aura ? [{ title: creature.aura.name, value: cc.GetAuraReach(creature.aura.reach).display }] : [],
+          },
+        ],
+      },
+      {
+        boxes: [
+          {
+            header: "Tesouros",
+            items: creature.treasures.map((t) => ({ title: t.name, value: tc.GetTreasureType(t.type).display })),
+          },
+        ],
+      },
+    ],
+    [
+      actionItems,
+      creature.armorClass,
+      creature.attributes.charisma,
+      creature.attributes.constitution,
+      creature.attributes.dexterity,
+      creature.attributes.intelligence,
+      creature.attributes.strength,
+      creature.attributes.wisdom,
+      creature.aura,
+      creature.class,
+      creature.conditionImmunities,
+      creature.customSpecials,
+      creature.damagesEffectiveness,
+      creature.environment,
+      creature.initiative,
+      creature.languages,
+      creature.legendaryResistences,
+      creature.movements.burrowing,
+      creature.movements.flying,
+      creature.movements.speed,
+      creature.movements.swimming,
+      creature.primaryAlignment,
+      creature.race,
+      creature.rarity,
+      creature.regeneration.amount,
+      creature.secondaryAlignment,
+      creature.secondaryClass,
+      creature.secondarySubClass,
+      creature.senses.blindSight,
+      creature.senses.darkVision,
+      creature.senses.tremorsense,
+      creature.senses.trueSight,
+      creature.size,
+      creature.subClass,
+      creature.treasures,
+      creature.type,
+      creature.weakSpots,
+      creatureAttacksUpdated,
+      creatureHPUpdated,
+      reactionItems,
+    ]
+  );
 
   async function OpenModalExport() {
     setModal(<ModalExport creature={creature} onClose={setModal} />);
@@ -300,30 +344,32 @@ function Summary({ creature, onSave, onDelete, isBasicPack }) {
         </div>
       </div>
       <div className="summary-fields">
-        {summaryRows.map((row, rowIndex) => (
-          <div className="summary-row" key={rowIndex}>
-            {row.boxes
-              .filter((box) => box.items.length > 0)
-              .map((box, boxIndex) => (
-                <div className="summary-box" key={boxIndex}>
-                  <header>
-                    <h5>{box.header}</h5>
-                  </header>
-                  <main className="summary-items">
-                    {box.items
-                      .filter((item) => item.value)
-                      .map((item, itemIndex) => (
-                        <div className="summary-item" key={itemIndex}>
-                          <span className="title">{item.title}</span>
-                          <span>{item.value}</span>
-                        </div>
-                      ))}
-                  </main>
-                  <footer>{box.footer}</footer>
-                </div>
-              ))}
-          </div>
-        ))}
+        {summaryRows
+          .filter((row) => row.boxes.some((b) => b.items.length > 0))
+          .map((row, rowIndex) => (
+            <div className="summary-row" key={rowIndex}>
+              {row.boxes
+                .filter((box) => box.items.length > 0)
+                .map((box, boxIndex) => (
+                  <div className="summary-box" key={boxIndex}>
+                    <header>
+                      <h5>{box.header}</h5>
+                    </header>
+                    <main className="summary-items">
+                      {box.items
+                        .filter((item) => item.value)
+                        .map((item, itemIndex) => (
+                          <div className="summary-item" key={itemIndex}>
+                            <span className="title">{item.title}</span>
+                            <span>{item.value}</span>
+                          </div>
+                        ))}
+                    </main>
+                    <footer>{box.footer}</footer>
+                  </div>
+                ))}
+            </div>
+          ))}
       </div>
     </div>
   );
