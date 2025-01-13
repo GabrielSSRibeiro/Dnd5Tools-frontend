5acee9fa2645740c44312e85b9b2f1e3b8595857
Gabriel Ribeiro <49738156+GabrielSSRibeiro@users.noreply.github.com>
2025-01-12 21:53:42 -0500

    desc updates

--- src/pages/Home/components/Map/components/ModalSuggestions/index.js
+++ src/pages/Home/components/Map/components/ModalSuggestions/index.js
@@ -34,11 +34,8 @@ function ModalSuggestions({ notes, onClose }) {
     "Continuar exercício até que essa localização e as próximas tenham 4-12 criaturas diferentes",
   ]);
   const mapSuggestions = useRef([
-    "Quando começando em um mar ou área aberta, tente criar primeiro, de forma simples, uma localização que engloba tudo que terá ali dentro, como por exemplo um continente",
-    "O nome da localização nao deve ter o tipo de terreno diretamente no nome e sim seu propósito ou fama",
-    "Quando criando uma localização que tem outras dentro dela, tente criar localizações de tipos de terreno diferentes. Sempre tenha um grande ponto de referencia visível por grande localização, como montanhas ou similar. Pense na rota principal de estrada do local, se alguma, e onde assentamentos estariam localizados",
-    "Quando colocar pontos em uma localização, sempre ter algum marco ou mudança de terreno com distância pequena como antecipação do ponto maior, o impacto de sua presença. Sempre coloque outros pontos menores com conexoes como elevaçoes, rios, ravinas e outros entre os marcos de modo que distâncias grande sempre tenham referências",
-    "Quando terminar de criar uma localização, prepare uma marcha com ritmo normal. A duraçao recomendada de uma marcha direta entre os marcos mais importantes sob essas condiçoes é de aproximadamente 8 horas",
+    "Quando começando em um mar ou área aberta, tente criar primeiro, de forma simples, uma localização que engloba tudo que terá ali dentro, como por exemplo um continente. O nome da localização nao deve ter o tipo de terreno diretamente no nome e sim seu propósito ou fama. Quando criando uma localização que tem outras dentro dela, tente criar localizações de tipos de terreno diferentes. Sempre tenha um grande ponto de referencia visível por grande localização, como montanhas ou similar. Pense na rota principal de estrada do local, se alguma, e onde assentamentos estariam localizados",
+    "Quando colocar pontos em uma localização, sempre ter algum marco ou mudança de terreno com distância pequena como antecipação do ponto maior, o impacto de sua presença. Sempre coloque outros pontos menores com conexoes como elevaçoes, rios, ravinas e outros entre os marcos de modo que distâncias grande sempre tenham referências. Quando terminar de criar uma localização, prepare uma marcha com ritmo normal. A duraçao recomendada de uma marcha direta entre os marcos mais importantes sob essas condiçoes é de aproximadamente 8 horas",
   ]);
   const dungeonSuggestions = useRef([
     "As salas serem boas é o segredo. Ter elementos como buracos, rochas e outros de tamanhos variados entre espaços variados",
